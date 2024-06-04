fadb8af454fda5b636497e68dbb385d8d1615b0b
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-03 21:04:06 -0300

    fazendo alteracoes no exercicio_11 e upando o exercicio_12

--- Exercicio_12/Exercicio_12.cs
+++ Exercicio_12/Exercicio_12.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 // Autor: Gabriel do Amaral Alves
 // Data: 03/06/24
-// Enunciado: 11) Escreva um programa que leia um número e verifique se ele está
-// no intervalo de 10 a 20 (inclusive).
+// Enunciado: 12) Escreva um programa que leia um número e verifique se ele é maior que 100.
+// Se não for, informe o dobro desse número.
 
-namespace Exercicio_11
+
+namespace Exercicio_12
 {
-    internal class Exercicio_11
+    internal class Exercicio_12
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Programa que verifica se um número está no intervalo de 10 a 20 (inclusive).");
+            Console.WriteLine("Programa que leia um número e verifique se ele é maior que 100. Se não for, dobra esse número.");
 
             Console.Write("Digite um número: ");
             string? inputNumero = Console.ReadLine();
@@ -23,13 +24,13 @@ namespace Exercicio_11
             // Converter string para int
             else if (int.TryParse(inputNumero, out int numero))
             {
-                if (numero >= 10 && numero <= 20)
+                if (numero > 100)
                 {
-                    Console.WriteLine("O número está no intervalo de 10 a 20.");
+                    Console.WriteLine("O número informado é maoir a 100");
                 }
                 else
                 {
-                    Console.WriteLine("O número NÃO está no intervalo de 10 a 20.");
+                    Console.WriteLine($"O número por não ser maior que 100 será dobrado agora ele tem o valor de: {numero * 2}");
                 }
             }
             else
